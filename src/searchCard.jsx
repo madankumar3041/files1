36f6bfa7cddb36314a1abd07e52c3d765b67d33b
madankumar3041 <69312892+madankumar3041@users.noreply.github.com>
2021-02-18 20:36:23 +0530

    Add files via upload

---
+++ src/searchCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Styles.css";
+export default function SearchCard({ data }) {
+  return (
+    <div className="display-record">
+      <td>  {data?.url || ""}
+      {data?.url ? <br /> : ""}</td>
+      <br></br>
+    
+      <td> {data?.product_name}</td>
+   
+      <br></br>
+      <td>  {data?.product_price}</td>
+    
+      <br></br>
+      <td>  {data?.availability_status}</td>
+    
+      <br></br>
+      <td> {data?.delivery_estimation}</td>
+     
+      <br></br>
+    </div>
+  );
+}
